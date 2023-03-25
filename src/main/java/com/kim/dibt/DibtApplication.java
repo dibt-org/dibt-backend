90d9eb09fecb3b0cd4e5232cfcb95edfbf0bdc8a
ismail.karaca <ismailkrc57@gmail.com>
2023-03-25 14:06:33 +0300

    initial commit

---
+++ src/main/java/com/kim/dibt/DibtApplication.java
@@ -0,0 +1,13 @@
+package com.kim.dibt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DibtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DibtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kim/dibt/DibtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kim.dibt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DibtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
