1c4bce9aa0493d11db438457fba065eb6821c2ca
ismail.karaca <ismailkrc57@gmail.com>
2023-03-27 16:49:48 +0300

    Messages Sabitleri eklendi

---
+++ src/main/java/com/kim/dibt/core/utils/constants/Messages.java
@@ -0,0 +1,13 @@
+package com.kim.dibt.core.utils.constants;
+
+public class Messages {
+    public static final String SUCCESS = "success";
+    public static final String FAIL = "fail";
+    public static final String NOT_FOUND = "not found";
+    public static final String ALREADY_EXISTS = "already exists";
+    public static final String DELETED = "deleted";
+    public static final String UPDATED = "updated";
+    public static final String ADDED = "added";
+    public static final String LISTED = "listed";
+
+}
