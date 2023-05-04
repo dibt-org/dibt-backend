e5ad2134ea44f793be752a6eed01d1ad1684b226
ismail.karaca <ismailkrc57@gmail.com>
2023-05-04 17:16:49 +0300

    Swagger Authentication için düzenlendi

--- src/main/java/com/kim/dibt/security/auth/AuthenticationController.java
+++ src/main/java/com/kim/dibt/security/auth/AuthenticationController.java
@@ -1,15 +1,16 @@
 package com.kim.dibt.security.auth;
 
+import com.kim.dibt.core.utils.constants.CoreConstants;
+import com.kim.dibt.core.utils.result.DataResult;
 import com.kim.dibt.core.utils.result.Result;
+import com.kim.dibt.core.utils.result.SuccessDataResult;
+import com.kim.dibt.security.dto.AuthenticationResponse;
+import com.kim.dibt.security.dto.LoginRequest;
+import com.kim.dibt.security.dto.RegisterRequest;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
-import com.kim.dibt.core.utils.constants.CoreConstants;
-import com.kim.dibt.core.utils.result.DataResult;
-import com.kim.dibt.core.utils.result.SuccessDataResult;
-import com.kim.dibt.security.dto.LoginRequest;
-import com.kim.dibt.security.dto.AuthenticationResponse;
-import com.kim.dibt.security.dto.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +25,7 @@ public class AuthenticationController {
 
     private final AuthenticationService service;
 
+    @SecurityRequirement(name = "none")
     @PostMapping("/register")
     public ResponseEntity<Result> register(
             @RequestBody @Valid RegisterRequest request
@@ -35,6 +37,7 @@ public class AuthenticationController {
         return ResponseEntity.ok(result);
     }
 
+    @SecurityRequirement(name = "none")
     @PostMapping("/login")
     public ResponseEntity<DataResult<AuthenticationResponse>> login(
             @RequestBody @Valid LoginRequest request
@@ -50,7 +53,7 @@ public class AuthenticationController {
         service.refreshToken(request, response);
     }
 
-    @PostMapping("/change-password")
+    @PatchMapping("/change-password")
     public ResponseEntity<Result> changePassword(
             @RequestParam String oldPassword,
             @RequestParam String newPassword,
